866bf16307eacc0928655c231c7233f23eda3fd6
Rafael Beraldo <rafael.pimpa@gmail.com>
2022-07-06 12:28:48 -0300

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Vite-Electron
+
+This is the source of the Dev.to tutorial in:
+https://dev.to/rafaelpimpa/integrate-vite2-with-electron-with-minimal-boilerplate-4af0-temp-slug-7966855
+
+## Install dependencies
+
+```
+npm install
+```
+
+## Run dev server
+
+```
+npm run electron:dev
+```
+
+## Build app
+
+```
+npm run electron:build
+```
---
+++ electron/main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow, shell } = require('electron')
+const { join } = require('path')
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+  process.exit(0)
+}
+
+let win = null
+
+async function createWindow () {
+  win = new BrowserWindow({
+    title: 'Main window',
+    icon: join(__dirname, '../dist/favicon.svg'),
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: join(__dirname, '../electron/preload.js'),
+      nodeIntegration: true
+    }
+  })
+
+  if (app.isPackaged) {
+    // win.removeMenu()
+    win.loadFile(join(__dirname, '../dist/index.html'))
+  } else {
+    win.loadURL('http://localhost:3000')
+    win.webContents.openDevTools()
+  }
+
+  // Make all links open with the browser, not with the application
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith('https:')) shell.openExternal(url)
+    return { action: 'deny' }
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  win = null
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('second-instance', () => {
+  if (win) {
+    // Focus on the main window if the user tried to open another
+    if (win.isMinimized()) win.restore()
+    win.focus()
+  }
+})
+
+app.on('activate', () => {
+  const allWindows = BrowserWindow.getAllWindows()
+  if (allWindows.length) {
+    allWindows[0].focus()
+  } else {
+    createWindow()
+  }
+})
